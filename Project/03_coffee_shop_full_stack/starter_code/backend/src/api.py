74ea4e6100a7d93d641d56755721ac39876599a0
James Agyabeng <ajagyabeng@gmail.com>
2022-10-03 11:41:36 +0000

    Write route to edit drink

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -68,17 +68,31 @@ def add_drink(jwt):
         abort(400)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def edit_drink(jwt, drink_id):
+    body = request.get_json()
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+    if not drink:
+        abort(404)
+    else:
+        try:
+            if 'title' in body:
+                drink.title = body.get('title')
+                drink.update()
+                return jsonify({
+                    'success': True,
+                    'drinks': drink.long()
+                })
+            elif 'recipe' in body:
+                drink.recipe = json.dumps(body.get('title'))
+                drink.update()
+                return jsonify({
+                    'success': True,
+                    'drinks': drink.long()
+                })
+        except:
+            abort(422)
 
 
 '''
